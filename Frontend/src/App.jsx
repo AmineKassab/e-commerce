3ccb18cd95b25765c6bab79435167fd52c6ae88a
Amine Kassab <nm_kassab@esi.dz>
2025-08-27 18:41:02 +0100

    second commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route } from "react-router";
 import Home from './pages/Home';
-import Collection from './pages/collection';
+import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Product from './pages/Product';
