12520d80d1e1002f9daaeeb3fe337aa6aac11db4
Amine Kassab <nm_kassab@esi.dz>
2025-08-28 16:08:43 +0100

    10 commit

--- Frontend/src/pages/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder.jsx
@@ -6,6 +6,8 @@ import { faStripe,faCcVisa } from '@fortawesome/free-brands-svg-icons'
 import { ShopContext } from '../context/ShopContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 
